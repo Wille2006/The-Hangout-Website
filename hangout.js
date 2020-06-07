e476506572e6168018fae9fdf1c45179b6c41f6e
Wille <willek06@icloud.com>
2020-06-08 01:12:42 +0200

    Version 1.1 Release
    Updated JavaScript logic for the only rule of The Hangout.

--- hangout.js
+++ hangout.js
@@ -1,7 +1,10 @@
 function helloworld() {
     var rule = prompt("What is the one and only rule?!");
 
-    if (rule.toLocaleLowerCase() == "dont be lame") {
+    if ((rule.toLocaleLowerCase() == "don't be lame") ||
+        (rule.toLocaleLowerCase() == "don't be lame!") ||
+        (rule.toLocaleLowerCase() == "dont be lame") ||
+        (rule.toLocaleLowerCase() == "dont be lame!")) {
         alert("You are, correct!");
     } else {
         alert("You are, incorrect!");
